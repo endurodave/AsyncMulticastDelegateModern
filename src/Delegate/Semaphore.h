e22855f930495d9eb7d49f2da0f1a1798348815a
BOOK-0CD3P8P8FE\endur <endurodave@gmail.com>
2024-12-10 06:28:48 -0800

    Make Delegate library header only

--- src/Delegate/Semaphore.h
+++ src/Delegate/Semaphore.h
@@ -6,6 +6,9 @@
 #include <mutex>
 #include <atomic>
 
+// Fix compiler error on Windows
+#undef max
+
 namespace DelegateLib {
 
 /// @brief A semaphore wrapper class. 
@@ -18,10 +21,41 @@ public:
 	/// Called to wait on a semaphore to be signaled.
 	/// @param[in] timeout - timeout in milliseconds
 	/// @return Return true if semaphore signaled, false if timeout occurred. 
-	bool Wait(std::chrono::milliseconds timeout);
+	bool Wait(std::chrono::milliseconds timeout)
+	{
+        std::unique_lock<std::mutex> lk(m_lock);
+        if (timeout == std::chrono::milliseconds::max())
+        {
+            m_sema.wait(lk, [this] { return m_signaled; });
+        }
+        else
+        {
+            if (!m_sema.wait_for(lk, timeout, [this] { return m_signaled; }))
+            {
+                return false; // Timeout occurred
+            }
+        }
+
+        if (m_signaled)
+        {
+            m_signaled = false;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+	}
 
 	/// Called to signal a semaphore.
-	void Signal();
+    void Signal()
+    {
+        {
+            std::unique_lock<std::mutex> lk(m_lock);
+            m_signaled = true;
+        }
+        m_sema.notify_one();
+    }
 
 private:
 	// Prevent copying objects
