d327f9592d2356f3722220f0250f2e5b0fc6384c
BOOK-0CD3P8P8FE\endur <endurodave@gmail.com>
2025-01-03 06:36:32 -0800

    Update doxygen

--- docs/html/navtreedata.js
+++ docs/html/navtreedata.js
@@ -24,64 +24,12 @@
 */
 var NAVTREE =
 [
-  [ "Delegates", "index.html", [
-    [ "Asynchronous Delegates in C++", "index.html#autotoc_md0", null ],
-    [ "Design Documentation", "index.html#autotoc_md1", null ],
-    [ "Quick Start", "index.html#autotoc_md2", [
-      [ "Basic Examples", "index.html#autotoc_md3", null ],
-      [ "Publish/Subscribe Example", "index.html#autotoc_md4", [
-        [ "Publisher", "index.html#autotoc_md5", null ],
-        [ "Subscriber", "index.html#autotoc_md6", null ]
-      ] ],
-      [ "All Delegate Types Example", "index.html#autotoc_md7", null ],
-      [ "Asynchronous API Example", "index.html#autotoc_md8", null ]
-    ] ],
-    [ "Delegate Classes", "index.html#autotoc_md9", null ],
-    [ "Project Build", "index.html#autotoc_md10", [
-      [ "Windows Visual Studio", "index.html#autotoc_md11", null ],
-      [ "Linux Make", "index.html#autotoc_md12", null ]
-    ] ],
-    [ "Related Repositories", "index.html#autotoc_md13", [
-      [ "Alternative Implementations", "index.html#autotoc_md14", null ],
-      [ "Projects Using Delegates", "index.html#autotoc_md15", null ]
-    ] ],
-    [ "Star History", "index.html#autotoc_md16", null ],
-    [ "Namespaces", "namespaces.html", [
-      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
-      [ "Namespace Members", "namespacemembers.html", [
-        [ "All", "namespacemembers.html", null ],
-        [ "Functions", "namespacemembers_func.html", null ],
-        [ "Variables", "namespacemembers_vars.html", null ]
-      ] ]
-    ] ],
-    [ "Classes", "annotated.html", [
-      [ "Class List", "annotated.html", "annotated_dup" ],
-      [ "Class Index", "classes.html", null ],
-      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
-      [ "Class Members", "functions.html", [
-        [ "All", "functions.html", "functions_dup" ],
-        [ "Functions", "functions_func.html", "functions_func" ],
-        [ "Typedefs", "functions_type.html", null ],
-        [ "Related Symbols", "functions_rela.html", null ]
-      ] ]
-    ] ],
-    [ "Files", "files.html", [
-      [ "File List", "files.html", "files_dup" ],
-      [ "File Members", "globals.html", [
-        [ "All", "globals.html", null ],
-        [ "Typedefs", "globals_type.html", null ],
-        [ "Macros", "globals_defs.html", null ]
-      ] ]
-    ] ]
-  ] ]
+  [ "Delegates", "index.html", ]
 ];
 
 var NAVTREEINDEX =
 [
-"_delegate_8h.html",
-"class_delegate_lib_1_1_delegate_function_3_01_ret_type_07_args_8_8_8_08_4.html#a1fded7c503030e142d45bfbeed84fbb7",
-"class_delegate_lib_1_1_delegate_member_async_3_01_t_class_00_01_ret_type_07_args_8_8_8_08_4.html#ab2dc309b92c007f22d9e22b489d53ff3",
-"class_delegate_lib_1_1heap__arg__deleter_3_01_t_01_5_01_4.html"
+"index.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
--- docs/html/search/searchdata.js
+++ docs/html/search/searchdata.js
@@ -1,42 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "abcdefghilmopqrstuvwx~",
-  1: "dhimsu",
-  2: "ds",
-  3: "dmrsu",
-  4: "abcdeghimoprstuw~",
-  5: "iw",
-  6: "bcdfmosx",
-  7: "o",
-  8: "bx",
-  9: "d"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "namespaces",
-  3: "files",
-  4: "functions",
-  5: "variables",
-  6: "typedefs",
-  7: "related",
-  8: "defines",
-  9: "pages"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Namespaces",
-  3: "Files",
-  4: "Functions",
-  5: "Variables",
-  6: "Typedefs",
-  7: "Friends",
-  8: "Macros",
-  9: "Pages"
 };
 
